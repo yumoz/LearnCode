1d06ece7fd6c066cd48bc2b684b34b51090faac9
yumoz <zealouszxl@163.com>
2020-11-11 22:43:44 +0800

    add some code for my analysis

--- AdvancedCCode/Day1_DataStore/datastore.c
+++ AdvancedCCode/Day1_DataStore/datastore.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-int check_sys();
-//{
-//	int i = 1;
-//	return (*(char *)&i);
-//}
+int check_sys()
+{
+	//0000 0000 0000 0000 0000 0000 0000 0001
+	//将i的低8个字节取出来
+	//如果低地址位存放的是0，那么是大端；否则是小段
+	int i = 1;
+	return (*(char *)&i);
+}
 
 int main()
 {
@@ -31,13 +34,15 @@ int main()
 	//printf("b=%d size=%d\n", b,sizeof(b));
 	return 0;
 }
-int check_sys()
-{
-	union
-	{
-		int i;
-		char c;
-	}un;
-	un.i = 1;
-	return un.c;
-}
+
+//联合体 后面尝试
+//int check_sys()
+//{
+//	union
+//	{
+//		int i;
+//		char c;
+//	}un;
+//	un.i = 1;
+//	return un.c;
+//}
