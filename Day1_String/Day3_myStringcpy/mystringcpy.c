eba03f91de8bb67c41f81a1be124f7cd0ca19b34
yumoz <zealouszxl@163.com>
2020-11-21 13:49:43 +0800

    add mystringcpy function

---
+++ Day1_String/Day3_myStringcpy/mystringcpy.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<windows.h>
+#include<assert.h>
+
+#pragma warning(disable:4996)
+
+char *MyStrcpy(char *dst, const char *str)
+{
+	assert(NULL != str);
+	assert(NULL != dst);
+	char *cp = dst;
+	while (*cp = *str){
+		cp++;
+		str++;
+	}
+	return dst;
+}
+int main()
+{
+	const char *arr = "abcdefg1234";
+	int len = sizeof(arr) - 1;
+	char RecStr[20] = { 0 };
+	printf("RecvString:%s\n", MyStrcpy(RecStr, arr));
+	system("pause");
+	return 0;
+
+}
