b674adae3fdd6f2c0a80d55fefbcdf91932a6fac
yumoz <zealouszxl@163.com>
2020-12-02 13:00:28 +0800

    change some files

--- Day2_Pointer/Day3_discriminate/mydiscriminate.c
+++ Day2_Pointer/Day3_discriminate/mydiscriminate.c
@@ -1,6 +1,74 @@
 #include<stdio.h>
 #include<windows.h>
 //
+
+int main()
+{
+	char *p = "abcdef";
+
+	printf("%d\n", sizeof(p));//指针大小
+
+	printf("%d\n", sizeof(p+1));//指针大小
+
+	printf("%d\n", sizeof(*p));//首元素占一个字节
+
+	printf("%d\n", sizeof(p[0]));//the same as up
+
+	printf("%d\n", sizeof(&p));//指针的地址，所以任然是4个
+
+	printf("%d\n", sizeof(&p+1));
+
+	printf("%d\n", sizeof(&p[0]+1));
+
+
+	//printf("%d\n", strlen(p));
+
+	//printf("%d\n", strlen(p + 1));
+
+	//printf("%d\n", strlen(*p));
+
+	//printf("%d\n", strlen(p[0]));
+
+	//printf("%d\n", strlen(&p));
+
+	//printf("%d\n", strlen(&p+1));
+
+	//printf("%d\n", strlen(&p[0]+1));
+
+
+
+
+
+	//二维数组
+
+	int a[3][4] = {0};
+
+	printf("%d\n",sizeof(a));
+
+	printf("%d\n", sizeof(a[0][0]));
+
+	printf("%d\n", sizeof(a[0]));
+
+	printf("%d\n", sizeof(a[0] + 1));
+
+	printf("%d\n", sizeof(*(a[0] + 1)));
+
+	printf("%d\n", sizeof(a + 1));
+
+	printf("%d\n", sizeof(*(a + 1)));
+
+	printf("%d\n", sizeof(&a[0] + 1));
+
+	printf("%d\n", sizeof(*(&a[0] + 1)));
+
+	printf("%d\n", sizeof(*a));
+
+	printf("%d\n", sizeof(a[3]));
+	system("pause");
+	return 0;
+}
+
+#if 0
 int main()
 {
 	//一维数组
@@ -18,3 +86,4 @@ int main()
 	system("pause");
 	return 0;
 }
+#endif
