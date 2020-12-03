a1d0e66a245c776a1ff6d1de1f8df6fe762d3db7
yumoz <zealouszxl@163.com>
2020-12-03 23:36:09 +0800

    add file io test

---
+++ Day5_FileIO/Day2_FileTest/testfile.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<windows.h>
+#pragma warning (disable:4996)
+
+int main()
+{
+	FILE * fp = fopen("test.txt", "wb");
+	long size;
+	if (fp == NULL)
+		perror("Error opening file");
+	else{
+		fputs("this is ", fp);
+		fseek(fp, 5, SEEK_SET);
+		fputs("Sam", fp);
+		size = ftell(fp);//返回文件指针相对于起始位置的偏移量
+		fclose(fp);
+		printf("Size is %ld bytes. \n", size);
+	}
+
+	
+
+	//for (char i = 'a'; i <= 'z'; i++){
+	//	fputc(i, fp);
+	//}
+
+	//fclose(fp);
+	fp = NULL;
+	system("pause");
+	return 0;
+}
--- Day5_FileIO/Day5_FileIO/testfile.c
+++ Day5_FileIO/Day5_FileIO/testfile.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
+/* rewind example */
+#include <stdio.h>
 #include<windows.h>
-#pragma warning (disable:4996)
-
+#pragma warning(disable:4996)
 int main()
 {
-	FILE * fp = fopen("test.txt", "w");
-	/*for (char i = 'a'; i <= 'z'; i++){
-		fputc(i, fp);
-	}*/
-	/*for (char i = 'a'; i <= 'z'; i++){
-		fputc(i,fp);
-	}*/
-	fgets(fp);
-	
-	fputs(i, stdout);
-	fclose(fp);
-	fp = NULL;
+	int n;
+	FILE * pFile;
+	char buffer[27];
+	int *a = "hello new thing";
+	pFile = fopen("myfile.txt", "w+");
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		fputc(n, pFile);//向文件写
+	}
+	rewind(pFile);//让文件指针的位置回到文件的起始位置
+	fread(buffer, 1, 26, pFile);
+	fclose(pFile);
+	buffer[26] = '\0';
+	puts(buffer);//打印到屏幕
 	system("pause");
 	return 0;
 }
